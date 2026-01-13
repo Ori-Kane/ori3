6f48a647c80368399677214f75e5e986d487d619
Ori-Kane <ori120910@gmail.com>
2026-01-13 12:44:42 +0200

    7m

---
+++ ori3/Ex22.js
@@ -0,0 +1,32 @@
+﻿//1Targil
+var div = document.getElementById("aElement");
+var aEl = document.createElement("a");
+aEl.setAttribute("href", "http://www.google.com");
+aEl.innerHTML = "GOOGLE";
+div.appendChild(aEl);
+//2Targil
+var div = document.getElementById("aElement");
+var img = document.createElement("img");
+img.setAttribute("src", "img/1.jpg");
+img.setAttribute("height", "200");
+img.setAttribute("width", "200");
+div.appendChild(img);
+//3Targil
+var div = document.getElementById("aElement");
+var table = document.createElement("table");
+var tr = document.createElement("tr");
+var td1 = document.createElement("td");
+var td2 = document.createElement("td");
+var img = document.createElement("img");
+img.setAttribute("src", "img/1.jpg");
+img.setAttribute("height", "200");
+img.setAttribute("width", "200");
+var aEl = document.createElement("a");
+td1.appendChild(img);
+td2.appendChild(aEl);
+tr.appendChild(td1);
+tr.appendChild(td2);
+table.appendChild(tr);
+div.appendChild(table);
+
+
---
+++ ori3/JavaScriptfirst.js
@@ -0,0 +1,13 @@
+﻿document.write("somthing");
+alert("somthing");
+var num = parseInt(prompt("enter a num", 7));
+var name = prompt("enter your name");
+var age = parseInt(prompt("enter your age", 0));
+document.write("<br/>")
+document.write("next year you will turn " + (age + 1));
+var num1 = parseInt(prompt("enter another num"));
+document.write(num + num1);
+document.write(num - num1);
+document.write(num * num1);
+document.write(num / num1);
+
